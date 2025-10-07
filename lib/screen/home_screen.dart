24645da081e3a3013c9f6947ca650441b0695fb9
SyedRaihanuzzaman <142418984+SyedRaihanuzzaman@users.noreply.github.com>
2025-10-08 04:12:45 +0600

    home screen design done .all item showing

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -18,6 +18,72 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Total Product is ${cartListData.length}"),
         backgroundColor: Colors.cyan,
       ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                  itemCount: products.length,
+
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                  ),
+
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 300,
+
+                      //   color: Colors.blue,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "${products[index].image}",
+                            height: 130,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "${products[index].name}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("৳ ${products[index].price}"),
+                              Icon(Icons.shopping_cart),
+                            ],
+                          ),
+
+                          Text(
+                            "${products[index].details}",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              MaterialButton(
+                onPressed: () {},
+                minWidth: double.infinity,
+                color: Colors.cyan,
+                child: Text("Go to Next Page"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
